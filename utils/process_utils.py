c477575378450e8f51515a1ef21dc9251fdc79cf
frica01 <819682099@qq.com>
2024-05-20 16:15:05 +0800

    optimize: 优化查找指定进程

--- utils/process_utils.py
+++ utils/process_utils.py
@@ -2,9 +2,11 @@
 # @Author : Frica01
 # @Time   : 2024/5/12 下午5:31
 # @Name   : process_utils.py
-import psutil
+
+
+import wmi
 
 
 def get_specific_process(proc_name: str = 'WeChat.exe') -> bool:
     """获取指定进程是否存在"""
-    return any(proc.name() == proc_name for proc in psutil.process_iter(attrs=['name']))
+    return any(process.Name == proc_name for process in wmi.WMI().Win32_Process(Name=proc_name))
